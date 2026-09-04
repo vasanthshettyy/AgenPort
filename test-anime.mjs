34eeb404e0a2189623d491a76b1ab1b68027adfc
Vasanth Shetty <vasanthvshetty55@gmail.com>
2026-09-04 09:52:52 +0530

    chore(cleanup): remove unused image assets and scratch scripts

--- test-anime.mjs
+++
@@ -1,5 +0,0 @@
-import * as animePkg from './node_modules/animejs/lib/anime.es.js';
-console.log('Keys in anime.es.js:', Object.keys(animePkg));
-if (animePkg.default) {
-  console.log('Keys in anime.es.js default export:', Object.keys(animePkg.default));
-}
